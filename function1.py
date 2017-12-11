e55287186cb0f578f9979b568a97bd7c627fe28e
rajula96reddy <rajula96reddy@gmail.com>
2017-12-11 16:05:15 +0530

    Added svmClassifier

--- function1.py
+++ function1.py
@@ -2,12 +2,14 @@ import numpy
 import numpy as np
 from sklearn import datasets
 import random
-
+import math
+from sklearn import svm
 iris = datasets.load_iris()
 # print iris
-X = iris.data[:, :2]
+X = iris.data
 # print X
 y = iris.target
+# y = np.array(y, dtype=np.float32)
 # print y
 def generate_abc(N_i):
     params_list=[]
@@ -23,14 +25,14 @@ def generate_svm(N_i):
     params_list=[]
     C = range(1, 10)
     Gamma = range(2, 5)
-    Max_iter = range(0,1)
+    Max_iter = range(1,2)
     Kernel = ['linear', 'poly', 'rbf']
     Decision_function_shape = ['ovr', 'ovo']
     Tol = [1e-7, 1e-4, 1e-6, 1e-8, 1e-5]
-    for i in range(1, N_i):
-        dict = {'C': C[random.randint(0, len(C)-1)],
-         'gamma': Gamma[random.randint(0, len(Gamma)-1)],
-         'max_iter': Max_iter[random.randint(0, len(Max_iter)-1)],
+    for i in range(1, N_i+1):
+        dict = {'C': float(C[random.randint(0, len(C)-1)]),
+         'gamma': float(Gamma[random.randint(0, len(Gamma)-1)]),
+         'max_iter': float(Max_iter[random.randint(0, len(Max_iter)-1)]),
          'kernel': Kernel[random.randint(0, len(Kernel)-1)],
          'decision_function_shape': Decision_function_shape[random.randint(0, len(Decision_function_shape)-1)],
          'tol': Tol[random.randint(0, len(Tol)-1)]}
@@ -46,7 +48,7 @@ def generate_DT(N_i):
     Max_leaf_nodes = range(2, 20)
     Max_features = ['auto', 'sqrt', 'log2', 'none']
     Min_impurity_split = [1e-7, 1e-4, 1e-6, 1e-8, 1e-5]
-    for i in range(1, N_i):
+    for i in range(1, N_i+1):
         dict = {'criterion': Criterion[random.randint(0, len(Criterion)-1)],
          'min_samples_split': Minimum_samples_split[random.randint(0, len(Minimum_samples_split)-1)],
          'max_depth': Max_depth[random.randint(0, len(Max_depth)-1)],
@@ -63,7 +65,7 @@ def generate_knn(N_i):
     Weights = ['distance', 'uniform']
     P = range(1, 5)
     Algorithm = ['brute', 'auto', 'ball_tree', 'kd_tree']
-    for i in range(1, N_i):
+    for i in range(1, N_i+1):
         dict = {'n_neighbors': N_neighbours[random.randint(0, len(N_neighbours)-1)],
          'weights': Weights[random.randint(0, len(Weights)-1)],
          'p': P[random.randint(0, len(P)-1)],
@@ -95,21 +97,49 @@ def generate_xgboost():
 P = [0.4, 0.4, 0.2]
 N = 10
 # def function1(A1, A2, A3, B, P, N):
-def function1(P, N):
+def GenParams(P, N):
         distribution = numpy.random.dirichlet(P)
         N_i = distribution * N
-        # for iter in range(0, len(N_i)):
-        #     N_i[iter] = int(round(N_i[iter]))
+        for iter in range(0, len(N_i)):
+             N_i[iter] = math.ceil(N_i[iter])
         N_i = np.array(N_i, dtype=np.int64)
         Chi = []
         print N_i
         Chi.append(generate_svm(N_i[0]))
         Chi.append(generate_knn(N_i[1]))
         Chi.append(generate_DT(N_i[2]))
-        print Chi
+        print Chi[0][0]
         Phi = generate_xgboost()
-        print Phi
+        # print Phi
         return Phi, N_i, Chi
-function1(P, N)
+
+Phi, N_Bold, Chi = GenParams(P, N)
 # print generate_DT(10)
 # print generate_xgboost(10)
+
+def Blend(L, Chi, N):
+    rho = 0.7
+    #Dfw=NUll
+    # for i in range(1,l+1):
+    sample_size = int(rho * len(X))
+    indices = range(0,len(X))
+    new_indices = random.sample(indices, sample_size)
+    # print new_indices
+    D_dash = X[new_indices]
+    Lables_dash = y[new_indices]
+    type(Lables_dash)
+    remaining_indices = list(set(indices) - set(new_indices))
+    D_complement = X[remaining_indices]
+    Labels_complement = y[remaining_indices]
+    # for i in range(0,3):
+    M=[]
+    for j in range(0,N[i]):
+    # print(Chi[0][0])
+    Object = svm.SVC()
+    Object.set_params(**Chi[0][0])
+    M.append(Object.fit(D_dash,Lables_dash))
+    # print(Object)
+    Object = svm.SVC()
+    Object.set_params(**Chi[0][0])
+    M.append(Object.fit(D_dash,Lables_dash))
+Blend(3, Chi, N_Bold)
