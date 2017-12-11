7450259375c02f5a34450fbf304bae6e51f9cb40
rajula96reddy <rajula96reddy@gmail.com>
2017-12-11 13:06:03 +0530

    Finished first function

--- function1.py
+++ function1.py
@@ -1,4 +1,5 @@
 import numpy
+import numpy as np
 from sklearn import datasets
 import random
 
@@ -22,13 +23,13 @@ def generate_svm(N_i):
     params_list=[]
     C = range(1, 10)
     Gamma = range(2, 5)
-    Max_iter = range()
+    Max_iter = range(0,1)
     Kernel = ['linear', 'poly', 'rbf']
     Decision_function_shape = ['ovr', 'ovo']
     Tol = [1e-7, 1e-4, 1e-6, 1e-8, 1e-5]
     for i in range(1, N_i):
-        dict = {'C': C[random.randint(0, len(weight)-1)],
-         'gamma': Gamma[random.randint(0, len(gamma)-1)],
+        dict = {'C': C[random.randint(0, len(C)-1)],
+         'gamma': Gamma[random.randint(0, len(Gamma)-1)],
          'max_iter': Max_iter[random.randint(0, len(Max_iter)-1)],
          'kernel': Kernel[random.randint(0, len(Kernel)-1)],
          'decision_function_shape': Decision_function_shape[random.randint(0, len(Decision_function_shape)-1)],
@@ -65,42 +66,50 @@ def generate_knn(N_i):
     for i in range(1, N_i):
         dict = {'n_neighbors': N_neighbours[random.randint(0, len(N_neighbours)-1)],
          'weights': Weights[random.randint(0, len(Weights)-1)],
-         'p': P[random.ran2dint(0, len(P)-1)],
+         'p': P[random.randint(0, len(P)-1)],
          'algorithm': Algorithm[random.randint(0, len(Algorithm)-1)], 'njobs': -1
          # 'leaf_size': Leaf_size[random.randint(0, len(Leaf_size)-1)]
          }
         params_list.append(dict)
     return params_list
 
-def generate_xgboost(N_i):
+def generate_xgboost():
     params_list=[]
     Max_depth = range(3, 10 + 1)#in case doesn't work inc step size to 2
     Booster = ['gbtree', 'gblinear' ]
     Objective = ['reg:logistic', 'reg:linear', 'binary:logistic']
     Min_child_weight = range(1, 6) #in case doesn't work inc step size to 2
     Learning_rate = numpy.arange(0, 0.5, 0.001)
-    print Learning_rate[3]
+    # print Learning_rate[3]
     Gamma = [gm/10 for gm in range(0, 5)]
-    for i in range(1, N_i):
-        dict = {'params': {'max_depth': Max_depth[random.randint(0, len(Max_depth)-1)],
+    # for i in range(1, N_i):
+    dict = {'params': {'max_depth': Max_depth[random.randint(0, len(Max_depth)-1)],
           'min_child_weight': Min_child_weight[random.randint(0, len(Min_child_weight)-1)],
           'gamma': Gamma[random.randint(0, len(Gamma)-1)],
           'booster': Booster[random.randint(0, len(Booster)-1)],
           'objective': Objective[random.randint(0, len(Objective)-1)],
           'learning_rate': Learning_rate[random.randint(0, len(Learning_rate)-1)]}}
-        params_list.append(dict)
-    return params_list
+    # params_list.append(dict)
+    return dict
 
-P=[0.4, 0.4, 0.2]
+P = [0.4, 0.4, 0.2]
 N = 10
-# print numpy.random.dirichlet(P)
-def function1(A1, A2, A3, B, P, N):
+# def function1(A1, A2, A3, B, P, N):
+def function1(P, N):
         distribution = numpy.random.dirichlet(P)
         N_i = distribution * N
+        # for iter in range(0, len(N_i)):
+        #     N_i[iter] = int(round(N_i[iter]))
+        N_i = np.array(N_i, dtype=np.int64)
         Chi = []
-        Chi.append(generate_svm(N_i))
-        Chi.append(generate_knn(N_i))
-        Chi.append(generate_DT(N_i))
-        Phi = generate_xgboost(N_i)
-print generate_DT(10)
-print generate_xgboost(10)
+        print N_i
+        Chi.append(generate_svm(N_i[0]))
+        Chi.append(generate_knn(N_i[1]))
+        Chi.append(generate_DT(N_i[2]))
+        print Chi
+        Phi = generate_xgboost()
+        print Phi
+        return Phi, N_i, Chi
+function1(P, N)
+# print generate_DT(10)
+# print generate_xgboost(10)
